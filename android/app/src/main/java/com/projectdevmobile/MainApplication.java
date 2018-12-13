a10d8d445c8caef90f03a283251bf5da10d7a53a
nolann.meleard@apitech.eu <nolann.meleard@apitech.eu>
2018-12-13 10:19:35 +0100

    Android compilation

--- android/app/src/main/java/com/projectdevmobile/MainApplication.java
+++ android/app/src/main/java/com/projectdevmobile/MainApplication.java
@@ -3,13 +3,12 @@ package com.projectdevmobile;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.airbnb.android.react.maps.MapsPackage;
-import com.oblador.vectoricons.VectorIconsPackage;
-import io.invertase.firebase.RNFirebasePackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
+import com.airbnb.android.react.maps.MapsPackage;
+import com.oblador.vectoricons.VectorIconsPackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -26,9 +25,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new MapsPackage(),
-            new VectorIconsPackage(),
-            new RNFirebasePackage()
+           new MapsPackage(),
+           new VectorIconsPackage()
       );
     }
 
